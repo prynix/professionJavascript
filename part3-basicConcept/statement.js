57ca1b7a9daba837cff25f19dd2e8c9ff180965d
sunhui-blog <2923132761@qq.com>
2019-12-03 14:12:01 +0800

    feat:statement

---
+++ part3-basicConcept/statement.js
@@ -0,0 +1,39 @@
+/*if语句
+建议用代码块
+condition 自动用Boolean()转换 
+*/
+if(1){}else{}
+
+/*do=while语句
+先执行后判断
+ */
+let i = 0; 
+do {
+  i++;
+  alert(i); // 1,2,3,4
+} while(i < 4); 
+
+/*while
+先判断后执行
+*/
+let i = 0;
+while( i<3 ) {
+  i++;
+  alert(i) // 1,2,3
+}
+
+/*for
+先判断后执行
+*/
+for(;;) {
+ //无限循环
+}
+
+// for循环转换为while
+for(;i < 3;) {
+
+}
+
+/*label
+
+*/
