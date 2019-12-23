e7aa7d3ca7b308d1a32854b53560be10fc31ba24
sunhui-blog <2923132761@qq.com>
2019-12-23 16:41:56 +0800

    feat:statement

--- part3-basicConcept/statement.js
+++ part3-basicConcept/statement.js
@@ -30,10 +30,79 @@ for(;;) {
 }
 
 // for循环转换为while
+let i = 0;
 for(;i < 3;) {
-
+  console.log(i)
+  i++
 }
 
 /*label
-
+代码中添加标签以使将来使用
+通常和for语句配合使用
 */
+end:
+for (let j=0;j<2;j++) {
+  for (let i=0;i<5;i++) {
+    // console.log(i) // 0,1,2
+    if (i==2){
+      break end //添加标签break不仅退出内部循环也会退出外部循环
+    }
+    console.log(i) // 0，1
+  }
+}
+
+for (let j=0;j<2;j++) {
+  for (let i=0;i<5;i++) {
+    // console.log(i) // 0,1,2,0,1,2
+    if (i==2){
+      break
+    }
+    console.log(i) // 0,1,0,1
+  }
+}
+
+end:
+for (let j=0;j<2;j++) {
+  for (let i=0;i<5;i++) {
+    // console.log(i) // 0,1,2,0,1,2
+    if (i==2){
+      continue end // 强制执行循环--退出内部循环，执行外部循环
+    }
+    console.log(i) // 0,1,0,1
+  }
+}
+
+for (let j=0;j<2;j++) {
+  for (let i=0;i<5;i++) {
+    if (i==2){
+      continue
+    }
+    console.log(i) // 0,1,3,4,0,1,3,4
+  }
+}
+
+/*with
+将代码的作用域设置到一个特定的对象中，简化多次编写同一个对象
+严格模式下不允许使用with，否则视为语法错误
+*/
+with (location) {
+  const url = href
+  const hostName = hostname
+  console.log(url)  // chrome-search://local-ntp/local-ntp.html
+  console.log(hostName) // local-ntp
+}
+
+/*switch
+ 多个case叠在一起写
+*/
+switch (status) {
+  case 0:
+  case -1:
+    console.log(0)
+    break
+  case 1 || 2:
+    console.log('success')
+    break
+  default:
+    console.log('error')
+}
