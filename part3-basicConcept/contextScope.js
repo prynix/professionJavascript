3eb4c99e079f98429707c22bee245923e9440832
sunhui-blog <2923132761@qq.com>
2020-01-03 16:39:26 +0800

    feat:scope

--- part3-basicConcept/contextScope.js
+++ part3-basicConcept/contextScope.js
@@ -78,3 +78,50 @@ function type() {}
 console.log(type instanceof Function) // true
 
 console.log(/^(\d+(,\s?\d+)*)$/ instanceof Function) // false
+
+// 执行环境及作用域
+
+/**
+ * 作用域链-向上搜索作用域链(向下搜索搜索不到)
+ *
+ * 与块作用域的不同：
+ * 1.将变量添加到当前的执行环境中；
+ * 2.for循环执行完毕后变量依旧会存在于循环外部的执行环境中
+ *
+ */
+if (true) {
+  const color = "blue"
+}
+
+console.log(color) // "blue"
+
+for (var i = 0; i < 10; i++) {
+  //..
+}
+console.log(i) // let声明局部变量i，此处取不到ie
+
+// 初始化变量前，一定要先声明，避免异常错误(严格模式抛出异常)
+
+// 向上搜索作用域链（一层层向上查找，搜索到就会自动停止）
+var color = 'red'
+function getColor() {
+  var color = 'blue'
+  console.log(color) // 'blue'
+}
+getColor()
+
+// 垃圾收集：标记清除&引用计数  关键在于采用的是哪种策略
+
+/*
+* 标记清除：将所有变量打上标记，“去掉”环境变量中使用的和变量引用的标记，加上准备删除变量的标记，垃圾回收器将标记的变量清除。
+* 引用计数：变量被引用次数进行计数，计数为0时清除计数。会引发内存泄漏。
+*/
+// 循环引用：将变量设置为null（BOM/DOM并不是原生Javascript对象，采用循环引用）
+const element = document.getElementById('id')
+const obj = new Object()
+element.obj = obj
+obj.element = element
+
+// 管理内存-将值设置为null释放引用，解除内存
+// 全局变量需要在不实用是手动解除引用，解除值的引用不意味着自动回收改制所占用的内存。解除引用的真正作用是让值脱离运行环境，在垃圾回收器下次运行时进行回收
+const gloabePerson = createPerson('lilei')
