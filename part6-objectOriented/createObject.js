c73b26fe9dbb46f03a03777ceb6745db30fb2a9f
sunhui-blog <2923132761@qq.com>
2020-03-05 16:07:53 +0800

    feat:createObject

--- part6-objectOriented/createObject.js
+++ part6-objectOriented/createObject.js
@@ -61,3 +61,48 @@ console.log(person2 instanceof Object) // true
 // 以这种方式定义的构造函数定义在Global中，浏览器在windows中
 
 window.Person // f () {...}
+
+// 1.把构造函数当作函数
+function Person(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = function () {
+    console.log(this.name)
+  }
+}
+
+let person1 = new Person('Nicholas', 21, 'Software Engineer')
+person1.sayName() // "Nicholas"
+
+// 当全局作用域中调用一个函数时，this对象总是指向Global对象
+Person('Nicholas', 21, 'Software Engineer')
+window.sayName() // "Nicholas"
+
+let o = new Object()
+Person.call(o, 'Nicholas', 21, 'Software Engineer')
+o.sayName() // "Nicholas"
+
+// 2.构造函数的问题
+console.log(person1.sayName === person2.sayName) // false
+
+// 构造函数创建函数，会导致不同的作用域链和标识符解析，但创建Function新实例的机制是相同的
+// 如下 定义全局函数可以解决，但缺点是对象要定义很多方法就会定义很多全局函数，自定义的引用类型就无封装性可言，此问题可以通过原型模式来解决
+
+function Person(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = sayName
+}
+
+// 全局函数
+function sayName () {
+  console.log(this.name)
+}
+
+let person1 = new Person('Nicholas', 21, 'Software Engineer')
+person1.sayName()
+
+// 原型模式
+
